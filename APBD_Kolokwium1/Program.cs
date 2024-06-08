a0d969ea07e1224567bf4e5bcd44e87144601ff1
SzymonSkorupkaS18295 <s18295@pjwstk.edu.pl>
2024-06-08 16:54:04 +0200

    Init - commit z pierwszą końcówką

---
+++ APBD_Kolokwium1/Program.cs
@@ -0,0 +1,26 @@
+using APBD_Kolokwium1.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<S18295Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers(); 
+app.Run();
